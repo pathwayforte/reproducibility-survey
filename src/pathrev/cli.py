218be2c5801fdc7986281c9e875766369c0aa93a
Daniel Domingo-Fernandez <danieldomingofernandez@hotmail.com>
2019-08-13 14:58:00 +0200

    Add code cli

--- src/pathrev/cli.py
+++ src/pathrev/cli.py
@@ -17,16 +17,31 @@ def main():
 
 @main.group()
 def gsea():
-    """Preprocessing utilities."""
+    """GSEA functionalities."""
 
 
 @gsea.command(help='Run GSEA')
-@click.option('-m', '--matrix', help="path to matrix", type=click.Path())
-@click.option('-c', '--cls', help="path to cls file", type=click.Path())
-@click.option('-g', '--gmt', help="path to gmt file", type=click.Path())
+@click.option(
+    '-m', '--matrix',
+    help="path to matrix",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    required=True
+)
+@click.option(
+    '-c', '--cls',
+    help="path to cls file",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    required=True
+)
+@click.option(
+    '-g', '--gmt',
+    help="path to gmt file",
+    type=click.Path(file_okay=True, dir_okay=False, exists=True),
+    required=True
+)
 def run(matrix, cls, gmt):
     """Run GSEA."""
-    NotImplemented
+    print("Hello")
 
 
 if __name__ == '__main__':
