8e32d3185c86e51eee675a7f014581690e4cd811
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2019-08-13 15:18:46 +0200

    Added the code in "run"

--- src/pathrev/cli.py
+++ src/pathrev/cli.py
@@ -3,7 +3,9 @@
 """Command line interface."""
 
 import logging
-
+import gseapy as gp
+import pandas as pd
+import numpy as np
 import click
 
 logger = logging.getLogger(__name__)
@@ -26,22 +28,56 @@ def gsea():
     help="path to matrix",
     type=click.Path(file_okay=True, dir_okay=False, exists=True),
     required=True
-)
+    )
 @click.option(
     '-c', '--cls',
     help="path to cls file",
     type=click.Path(file_okay=True, dir_okay=False, exists=True),
     required=True
-)
+    )
 @click.option(
     '-g', '--gmt',
     help="path to gmt file",
     type=click.Path(file_okay=True, dir_okay=False, exists=True),
     required=True
-)
+    )
 def run(matrix, cls, gmt):
     """Run GSEA."""
-    print("Hello")
+    click.echo("Start")
+    Miniml_data = pd.read_pickle("data/Miniml_Data.pkl")
+    Miniml_data.iloc[1, :].isnull().values.any()
+    Miniml_data.set_index('ID', inplace=True)
+    click.echo('Reading the matrix file')
+    df = pd.read_csv(matrix, sep='\t', header=0)
+    click.echo('Processing the matrix file')
+    df['ID_REF'] = df['ID_REF'].astype(str)
+    for i in df.index:
+        id_ref = df.at[i, 'ID_REF']
+        try:
+            df.at[i, 'ID_REF'] = Miniml_data.at[int(id_ref), 'GENE_SYMBOL']
+        except KeyError:
+            df.at[i, 'ID_REF'] = '---'
+    # if df.ID_REF.nunique() == 1:
+    # continue
+    new_index = sorted(df.columns[1:], key=lambda col_name: int(col_name[12:-4]))
+    new_index.insert(0, 'ID_REF')
+    df = df.reindex(new_index, axis=1)
+    click.echo('Running GSEA')
+    gs_res = gp.gsea(
+        data=df,  # or data='./P53_resampling_data.txt'
+        gene_sets=gmt,  # enrichr library names
+        cls=cls,  # cls=class_vector
+        # set permutation_type to phenotype if samples >=15
+        permutation_type='phenotype',
+        permutation_num=10,  # reduce number to speed up test
+        outdir=None,  # do not write output to disk
+        no_plot=True,  # Skip plotting
+        method='signal_to_noise',
+        processes=4,
+        format='png',
+    )
+    print(gs_res.res2d.index)
+    click.echo('End')
 
 
 if __name__ == '__main__':
