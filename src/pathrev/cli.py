b33e8434b1bc0bd74aaafc820d7920f41ba4db50
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2019-08-22 12:35:47 +0200

    Changed the run script for more generalized analysis

--- src/pathrev/cli.py
+++ src/pathrev/cli.py
@@ -42,33 +42,33 @@ def gsea():
     type=click.Path(file_okay=True, dir_okay=False, exists=True),
     required=True
     )
-@click.option(
-    '-p', '--pkl',
-    help="path to Miniml pkl",
-    type=click.Path(file_okay=True, dir_okay=False, exists=True),
-    required=True
-    )
-def run(matrix, cls, gmt, pkl):
+# @click.option(
+#     '-p', '--pkl',
+#     help="path to Miniml pkl",
+#     type=click.Path(file_okay=True, dir_okay=False, exists=True),
+#     required=True
+#     )
+def run(matrix, cls, gmt):
     """Run GSEA."""
-    click.echo("Run {} with {}, {}, {}".format(matrix, cls, gmt, pkl))
-    Miniml_data = pd.read_pickle(pkl)
-    Miniml_data.iloc[1, :].isnull().values.any()
-    Miniml_data.set_index('ID', inplace=True)
-    click.echo('Reading the matrix file')
+    click.echo("Run {} with {}, {}, {}".format(matrix, cls, gmt))
+    # Miniml_data = pd.read_pickle(pkl)
+    # Miniml_data.iloc[1, :].isnull().values.any()
+    # Miniml_data.set_index('ID', inplace=True)
+    # click.echo('Reading the matrix file')
     df = pd.read_csv(matrix, sep='\t', header=0)
-    click.echo('Processing the matrix file')
-    df['ID_REF'] = df['ID_REF'].astype(str)
-    for i in df.index:
-        id_ref = df.at[i, 'ID_REF']
-        try:
-            df.at[i, 'ID_REF'] = Miniml_data.at[int(id_ref), 'GENE_SYMBOL']
-        except KeyError:
-            df.at[i, 'ID_REF'] = '---'
-    # if df.ID_REF.nunique() == 1:
-    # continue
-    new_index = sorted(df.columns[1:], key=lambda col_name: int(col_name[12:-4]))
-    new_index.insert(0, 'ID_REF')
-    df = df.reindex(new_index, axis=1)
+    # click.echo('Processing the matrix file')
+    # df['ID_REF'] = df['ID_REF'].astype(str)
+    # for i in df.index:
+    #     id_ref = df.at[i, 'ID_REF']
+    #     try:
+    #         df.at[i, 'ID_REF'] = Miniml_data.at[int(id_ref), 'GENE_SYMBOL']
+    #     except KeyError:
+    #         df.at[i, 'ID_REF'] = '---'
+    # # if df.ID_REF.nunique() == 1:
+    # # continue
+    # new_index = sorted(df.columns[1:], key=lambda col_name: int(col_name[12:-4]))
+    # new_index.insert(0, 'ID_REF')
+    # df = df.reindex(new_index, axis=1)
     click.echo('Running GSEA')
     gs_res = gp.gsea(
         data=df,  # or data='./P53_resampling_data.txt'
@@ -83,7 +83,6 @@ def run(matrix, cls, gmt, pkl):
         processes=4,
         format='png',
     )
-    print(gs_res.res2d.index)
     gs_res.res2d.to_csv('gsea_result.tsv', sep='\t')
     gs_res.res2d.to_pickle('gsea_result_res2d.pkl')
     gs_res.heatmat.to_pickle('gsea_result_heatmat.pkl')
