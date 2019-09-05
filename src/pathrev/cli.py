935905da93d4c82679115efa1aa27d082a5be4fa
Vinay Bharadhwaj <vinaysbharadhwaj@gmail.com>
2019-09-05 12:38:27 +0200

    Added functionality to deseq2 to remove consequent 0's in the data; Cleaned commented code in cli

--- src/pathrev/cli.py
+++ src/pathrev/cli.py
@@ -42,33 +42,10 @@ def gsea():
     type=click.Path(file_okay=True, dir_okay=False, exists=True),
     required=True
     )
-# @click.option(
-#     '-p', '--pkl',
-#     help="path to Miniml pkl",
-#     type=click.Path(file_okay=True, dir_okay=False, exists=True),
-#     required=True
-#     )
 def run(matrix, cls, gmt):
     """Run GSEA."""
     click.echo("Run {} with {}, {}".format(matrix, cls, gmt))
-    # Miniml_data = pd.read_pickle(pkl)
-    # Miniml_data.iloc[1, :].isnull().values.any()
-    # Miniml_data.set_index('ID', inplace=True)
-    # click.echo('Reading the matrix file')
     df = pd.read_csv(matrix, sep='\t', header=0, index_col=0)
-    # click.echo('Processing the matrix file')
-    # df['ID_REF'] = df['ID_REF'].astype(str)
-    # for i in df.index:
-    #     id_ref = df.at[i, 'ID_REF']
-    #     try:
-    #         df.at[i, 'ID_REF'] = Miniml_data.at[int(id_ref), 'GENE_SYMBOL']
-    #     except KeyError:
-    #         df.at[i, 'ID_REF'] = '---'
-    # # if df.ID_REF.nunique() == 1:
-    # # continue
-    # new_index = sorted(df.columns[1:], key=lambda col_name: int(col_name[12:-4]))
-    # new_index.insert(0, 'ID_REF')
-    # df = df.reindex(new_index, axis=1)
     click.echo('Running GSEA')
     gs_res = gp.gsea(
         data=df,  # or data='./P53_resampling_data.txt'
